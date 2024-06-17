3280d6a1dea08e56b7d85047b5c5548d64383480
conradcl <conradcl345@gmail.com>
2024-06-17 13:23:32 -0400

    Add files via upload

--- generation.cpp
+++ generation.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
-#include <cctype>
-#include <vector>
+#include <iomanip>
+#include <sstream>
+#include <string>
 #include <fstream>
+#include <openssl/sha.h>
 
 using namespace std;
 
 void generation(int length, char password[], int index, ofstream& outfile);
 void capitalization(int length, char password[], ofstream& outfile);
-
-//these binary arrays are what are used to generate all possible capitalizatoin
-//arrangments for each "password", the 1s convert the letter to capital
+string sha256(const string& str);
 
 const char binaryArraysThree[][4] = {
     "111", "001", "011", "101",
@@ -23,14 +23,10 @@ const char binaryArraysFour[][5] = {
     "1100", "1101", "1110"
 };
 
-
-
 int main() {
-    //max length variable is changed for testing purposes
     const int maxLength = 4;
     char password[maxLength + 1] = { 0 };
 
-    //below there are commented lines that will place the output into a .txt file.
     ofstream outfile("output.txt");
 
     for (int length = 1; length <= maxLength; length++) {
@@ -41,10 +37,12 @@ int main() {
     return 0;
 }
 
-void generation(int length, char password[], int index, ofstream &outfile) {
+void generation(int length, char password[], int index, ofstream& outfile) {
     if (index == length) {
-        cout << password << endl;
-        //outfile << password << endl;
+        string pass(password);
+        string hash = sha256(pass);
+        cout << pass << " " << hash << endl;
+        outfile << pass << " " << hash << endl;
         capitalization(length, password, outfile);
         return;
     }
@@ -58,22 +56,30 @@ void generation(int length, char password[], int index, ofstream &outfile) {
 void capitalization(int length, char password[], ofstream& outfile) {
     if (length == 1) {
         password[0] = toupper(password[0]);
-        cout << password << endl;
-        //outfile << password << endl;
+        string pass(password);
+        string hash = sha256(pass);
+        cout << pass << " " << hash << endl;
+        outfile << pass << " " << hash << endl;
     }
     else if (length == 2) {
         password[0] = toupper(password[0]);
-        cout << password << endl;
-        //outfile << password << endl;
+        string pass(password);
+        string hash = sha256(pass);
+        cout << pass << " " << hash << endl;
+        outfile << pass << " " << hash << endl;
         password[0] = tolower(password[0]);
-        
+
         password[1] = toupper(password[1]);
-        cout << password << endl;
-        //outfile << password << endl;
+        pass = string(password);
+        hash = sha256(pass);
+        cout << pass << " " << hash << endl;
+        outfile << pass << " " << hash << endl;
 
         password[0] = toupper(password[0]);
-        cout << password << endl;
-        //outfile << password << endl;
+        pass = string(password);
+        hash = sha256(pass);
+        cout << pass << " " << hash << endl;
+        outfile << pass << " " << hash << endl;
 
         password[0] = tolower(password[0]);
         password[1] = tolower(password[1]);
@@ -88,8 +94,10 @@ void capitalization(int length, char password[], ofstream& outfile) {
                     password[j] = tolower(password[j]);
                 }
             }
-            cout << password << endl;
-            //outfile << password << endl;
+            string pass(password);
+            string hash = sha256(pass);
+            cout << pass << " " << hash << endl;
+            outfile << pass << " " << hash << endl;
         }
         password[0] = tolower(password[0]);
         password[1] = tolower(password[1]);
@@ -105,27 +113,29 @@ void capitalization(int length, char password[], ofstream& outfile) {
                     password[j] = tolower(password[j]);
                 }
             }
-            cout << password << endl;
-            //outfile << password << endl;
+            string pass(password);
+            string hash = sha256(pass);
+            cout << pass << " " << hash << endl;
+            outfile << pass << " " << hash << endl;
         }
         password[0] = tolower(password[0]);
         password[1] = tolower(password[1]);
         password[2] = tolower(password[2]);
         password[3] = tolower(password[3]);
     }
-    /*
-    else if (length == 4) {
-        for (int i = 0; i < 16; i++) {
-            for (int j = 0; j < 4; j++) {
-                if (i & (1 << j)) {
-                    password[j] = toupper(password[j]);
-                }
-                else {
-                    password[j] = tolower(password[j]);
-                }
-            }
-            cout << password << endl;
-        }
+}
+
+string sha256(const string& str)
+{
+    unsigned char hash[SHA256_DIGEST_LENGTH];
+    SHA256_CTX sha256;
+    SHA256_Init(&sha256);
+    SHA256_Update(&sha256, str.c_str(), str.size());
+    SHA256_Final(hash, &sha256);
+    stringstream ss;
+    for (int i = 0; i < SHA256_DIGEST_LENGTH; i++)
+    {
+        ss << hex << setw(2) << setfill('0') << (int)hash[i];
     }
-    */
+    return ss.str();
 }
